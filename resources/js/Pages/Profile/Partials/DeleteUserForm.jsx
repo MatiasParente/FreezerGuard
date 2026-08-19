ebc36745c6e92484aac3a743425d4f2e73a32f15
Matías Parente <matias.parente@estudiantes.utec.edu.uy>
2026-08-18 21:53:22 -0300

    cambios en el perfil

--- resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
@@ -49,32 +49,31 @@ export default function DeleteUserForm({ className = '' }) {
         <section className={`space-y-6 ${className}`}>
             <header>
                 <h2 className="text-lg font-medium text-gray-900">
-                    Delete Account
+                    Eliminar cuenta
                 </h2>
 
                 <p className="mt-1 text-sm text-gray-600">
-                    Once your account is deleted, all of its resources and data
-                    will be permanently deleted. Before deleting your account,
-                    please download any data or information that you wish to
-                    retain.
+                    Una vez que tu cuenta sea eliminada, todos sus recursos y datos
+                    serán eliminados permanentemente. Antes de eliminar tu cuenta,
+                    por favor descarga cualquier información que desees conservar.
                 </p>
             </header>
 
             <DangerButton onClick={confirmUserDeletion}>
-                Delete Account
+                Eliminar cuenta
             </DangerButton>
 
             <Modal show={confirmingUserDeletion} onClose={closeModal}>
                 <form onSubmit={deleteUser} className="p-6">
                     <h2 className="text-lg font-medium text-gray-900">
-                        Are you sure you want to delete your account?
+                        ¿Estás seguro de que deseas eliminar tu cuenta?
                     </h2>
 
                     <p className="mt-1 text-sm text-gray-600">
-                        Once your account is deleted, all of its resources and
-                        data will be permanently deleted. Please enter your
-                        password to confirm you would like to permanently delete
-                        your account.
+                        Una vez que tu cuenta sea eliminada, todos sus recursos y datos
+                        serán eliminados permanentemente. Por favor ingresa tu
+                        contraseña para confirmar que deseas eliminar permanentemente
+                        tu cuenta.
                     </p>
 
                     <div className="mt-6">
@@ -106,11 +105,11 @@ export default function DeleteUserForm({ className = '' }) {
 
                     <div className="mt-6 flex justify-end">
                         <SecondaryButton onClick={closeModal}>
-                            Cancel
+                            Cancelar
                         </SecondaryButton>
 
                         <DangerButton className="ms-3" disabled={processing}>
-                            Delete Account
+                            Eliminar cuenta
                         </DangerButton>
                     </div>
                 </form>
